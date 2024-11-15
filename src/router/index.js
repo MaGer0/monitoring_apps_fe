23d9473fa427dbae0d5be4040973882400fdfc71
Alfonsius Tolan Hera <alfontolan4@gmail.com>
2024-11-15 15:17:31 +0700

    Mengatur route agar lebih tertata dan menambah route untuk landing-pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
+import LandingPage from '@/views/LandingPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/start',
+      name: 'landing-pages',
+      component: LandingPage
+    },
     {
       path: '/login',
       name: 'login',
       component: LoginView
     },
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: HomeView,
     },
@@ -23,7 +29,21 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/',
+      redirect: '/start'
+    }
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.name === 'login' && localStorage.getItem('token')) {
+    next({ name: 'home' })
+  } else if (to.name !== 'login' && to.name !== 'landing-pages' && !localStorage.getItem('token')) {
+    next({ name: 'login' })
+  } else {
+    next()
+  }
 })
 
 export default router
