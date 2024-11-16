d57719b99882aed33d3665e2ad7a1f5a7c9cf85b
Alfonsius Tolan Hera <alfontolan4@gmail.com>
2024-11-16 09:09:02 +0700

    Mengatur tampilan Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,25 @@
 <template>
-  
+  <AppSidebar/>
+  <div id="home" data-aos="fade-up" class="container d-flex justify-content-center align-items-center vh-100">
+    <h1 class="text-center">Welcome!</h1>
+  </div>
 </template>
 
 <script>
+import Aos from 'aos';
+import "aos/dist/aos.css";
+import AppSidebar from '@/components/AppSidebar.vue';
 export default {
-  name: "HomeView"
+  name: "HomeView",
+  components: {
+    AppSidebar
+  },
+  mounted() {
+    Aos.init({
+      duration: 1000,
+      once: true
+    })
+  }
 }
 </script>
 
