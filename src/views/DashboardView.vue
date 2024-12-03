a773f6ba47f297dc8849b174bc4190af7443bd64
kaysannn <kaysannwflp1@gmail.com>
2024-12-03 10:40:25 +0700

    Menambah fitur delete data untuk tabel monitoring

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -19,7 +19,7 @@
           </button>
         </div>
       </div>
-      
+
       <ExportModules ref="exportModules" />
 
       <CreateModal
@@ -87,7 +87,10 @@
                         </button>
                       </li>
                       <li>
-                        <button class="btn p-2 w-100">
+                        <button
+                          class="btn p-2 w-100"
+                          @click="deleteMonitoring(data.id)"
+                        >
                           <i class="bi bi-trash fs-5"></i>
                         </button>
                       </li>
@@ -145,7 +148,10 @@
                     </button>
                   </li>
                   <li>
-                    <button class="btn p-2 w-100">
+                    <button
+                      @click="deleteMonitoring(data.id)"
+                      class="btn p-2 w-100"
+                    >
                       <i class="bi bi-trash fs-5"></i>
                     </button>
                   </li>
@@ -259,6 +265,24 @@ export default {
       } catch (error) {
         console.error("Error fetching data:", error);
       }
+    },
+    deleteMonitoring(id) {
+      console.log(id);
+      const token = "Bearer " + localStorage.getItem("token")
+
+      axios
+      .delete("http://127.0.0.1:8000/api/monitorings/" + id, {
+        headers: {
+          Authorization: token
+        }
+      })
+      .then((response) => {
+        console.log(response)
+        this.fetchData()
+      })
+      .catch((error) => {
+        console.log(error)
+      })
     },
     openCreateModal() {
       this.showModal = true;
