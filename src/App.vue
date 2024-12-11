0525cf19c6e9d63641f9caad3853dd2040588540
Alfonsius Tolan Hera <alfontolan4@gmail.com>
2024-12-11 14:32:59 +0700

    Consume API untuk donwload format import, serta sedikit penyesuaian di App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,19 @@
 
 <script>
 export default {
-  name: "App"
-}
+  name: "App",
+};
 </script>
 
 <style>
-
+.app {
+  background-image: url("https://img.freepik.com/free-vector/gradient-white-monochrome-background_23-2149017048.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position-y: bottom;
+  background-position-x: center;
+  position: relative;
+  overflow: hidden;
+  height: 100vh;
+}
 </style>
